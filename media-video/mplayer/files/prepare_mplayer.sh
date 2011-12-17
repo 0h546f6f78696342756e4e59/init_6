55248b81ae23867a555cd7d58f73e97f91dd8aec
Andrey Ovcharov (init_6) <sudormrfhalt@gmail.com>
2011-12-17 15:45:56 +0200

    Bump media-video/mplayer-1.0_rc4_p20111215

--- media-video/mplayer/files/prepare_mplayer.sh
+++ media-video/mplayer/files/prepare_mplayer.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 VERSION=$(date +%Y%m%d)
 PACKAGE="mplayer-1.0_rc4_p${VERSION}"
+DUMP_FFMPEG="$(dirname $0)/dump_ffmpeg.sh"
 
 svn checkout svn://svn.mplayerhq.hu/mplayer/trunk ${PACKAGE}
 
 pushd ${PACKAGE} > /dev/null
 	# ffmpeg is in git now so no svn external anymore
 	rm -rf ffmpeg
-	git clone git://git.libav.org/libav.git ffmpeg/
-	STORE_VERSION=$(svn log -r HEAD -q |grep ^r |cut -d' ' -f1)
-	echo "*** Remember to adjust mplayer ebuild with revision: \"SVN-${STORE_VERSION}\" ***"
+	git clone git://git.videolan.org/ffmpeg.git ffmpeg/
+        sh "$DUMP_FFMPEG"
+	STORE_VERSION=$(LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2)
+	printf "$STORE_VERSION" > snapshot_version
 popd > /dev/null
 
 find "${PACKAGE}" -type d -name '.svn' -prune -print0 | xargs -0 rm -rf
